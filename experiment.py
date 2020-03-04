6a36293c570218a4aaa714ba6fdd505ca66e98c4
Lucas <lucashmccabe@gmail.com>
2020-03-04 16:57:03 -0500

    Added default params, virus attribute, and count_infected function.

--- experiment.py
+++ experiment.py
@@ -8,7 +8,7 @@ class Experiment():
     Simulate an epidemic over a random graph.
     '''
 
-    def __init__(self, population:int, p_partner:float):
+    def __init__(self, population:int = 10, p_partner:float = 0.5, p_infect: float = 0.02):
         '''
         Constructor for the Experiment class.
         Initializes world for experimentation.
@@ -16,6 +16,8 @@ class Experiment():
         Arguments:
             population: the number of people/nodes to simulate
             p_partner: the probability of two nodes being sexual partners
+            p_infect: probability that an infected person will infect their
+                partner during an encounter.
         Raises:
             ValueError: if population is not >0
             ValueError: if p_partner is not in [0,1]
@@ -30,6 +32,7 @@ class Experiment():
 
         self.partners = self.init_partners(population, p_partner)
         self.infected = self.init_infected(population)
+        self.virus = Virus(p_infect)
 
 
     def init_partners(self, population:int, p_partner:float):
@@ -66,3 +69,11 @@ class Experiment():
         infected = np.zeros(population)
         infected[random.randint(0,population)] = 1
         return infected
+
+    def count_infected(self):
+        '''
+        Counts the number of infected individuals.
+        '''
+        return np.sum(self.infected)
+
+    #def simulate_step(self):
